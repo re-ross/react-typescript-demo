f14125939253d673a763772ea5d066aea70d7a11
Ryan Ross <ryan@reross.com>
2022-03-23 16:06:16 -0500

    handling event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import "./App.css";
-import Status from "./components/Status";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import Greet from "./components/Greet";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 function App() {
   return (
     <div className="App">
-      <Status status="loading" />
-      <Heading>Placeholder Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to DiCaprio!</Heading>
-      </Oscar>
-      <Greet name="Ryan" isLoggedIn={false} />
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked!", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
